875e0f613651649db01ea9699b93b3115d0f7cb2
A.E Clarence <33183127+clarnx@users.noreply.github.com>
2022-11-17 15:51:08 +0000

    Update timeout.js

--- netlify/functions/timeout.js
+++ netlify/functions/timeout.js
@@ -3,7 +3,7 @@ exports.handler = async function (event, context) {
  setTimeout(() => {
  return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Function was returned after 5 minutes }),
+    body: JSON.stringify({ message: "Function was returned after 5 minutes" }),
   };
  }, 300000)
   
