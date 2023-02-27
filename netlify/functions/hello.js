740ce711cac48bbcea12193da4a32b2bf02117be
A.E Clarence <33183127+clarnx@users.noreply.github.com>
2023-02-27 22:37:24 +0000

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,22 +1,22 @@
-// exports.handler = async function (event, context) {
-//   console.log(process.version)
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({ message: process.version }),
-//   };
-// };
-
-
-import fetch from "node-fetch";
-
-const API_ENDPOINT = "http://colormind.io/api/";
-
-exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
-    .then((response) => response.json())
-    .then((data) => ({
-      statusCode: 200,
-      body: data,
-    }))
-    .catch((error) => ({ statusCode: 422, body: String(error) }));
+exports.handler = async function (event, context) {
+  console.log(process.version)
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: process.version }),
+  };
 };
+
+
+// import fetch from "node-fetch";
+
+// const API_ENDPOINT = "http://colormind.io/api/";
+
+// exports.handler = async (event, context) => {
+//   return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
+//     .then((response) => response.json())
+//     .then((data) => ({
+//       statusCode: 200,
+//       body: data,
+//     }))
+//     .catch((error) => ({ statusCode: 422, body: String(error) }));
+// };
