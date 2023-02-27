f371e326d5942d8c9c6c53973767ee306ee8547d
A.E Clarence <33183127+clarnx@users.noreply.github.com>
2023-02-27 22:19:27 +0000

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,22 @@
-exports.handler = async function (event, context) {
-  console.log(process.version)
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: process.version }),
-  };
+// exports.handler = async function (event, context) {
+//   console.log(process.version)
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({ message: process.version }),
+//   };
+// };
+
+
+const fetch = require("node-fetch");
+
+const API_ENDPOINT = "http://colormind.io/api/";
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
+    .then((response) => response.json())
+    .then((data) => ({
+      statusCode: 200,
+      body: data,
+    }))
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
